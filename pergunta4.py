8c80f75d0dd8731d4ae3a06873b57a5f38fcac2a
Marcus Filipi Lehmkuhl <116232636+MarcusLehmkuhl@users.noreply.github.com>
2024-03-06 17:53:27 -0300

    Update pergunta4.py

--- pergunta4.py
+++ pergunta4.py
@@ -2,4 +2,7 @@
 
     Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?  """
 
- #Resposta: Para saber qual interruptor de cada lampada eu deixo uma lampada ligada durante o máximo de tempo possível e as outras duas lampadas desligadas, antes de checar a sala eu apagaria a lampada inicialmente ligada e acenderia uma entre as que estavam inicialmente apagadas, ao ir até a sala eu checaria qual lampada desligada está quente, assim a que estiver quente seria a primeira acesa, já a que estiver ligada, seria a segunda lampada e a que estiver apaga, foi a lampada que neste cenário, nunca foi acesa.
+# Resposta: Para saber qual interruptor de cada lampada eu deixo uma lampada ligada durante o máximo de tempo possível e as outras duas lampadas desligadas, 
+# antes de checar a sala eu apagaria a lampada inicialmente ligada e acenderia uma entre as que estavam inicialmente apagadas, 
+# ao ir até a sala eu checaria qual lampada desligada está quente, assim a que estiver quente seria a primeira acesa, já a que estiver ligada, seria a segunda lampada e a que estiver apaga, 
+# foi a lampada que neste cenário, nunca foi acesa.
